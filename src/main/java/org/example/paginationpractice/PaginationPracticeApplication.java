463d2dd8e0f205412c56e26c8814d62845be9fe6
Chan-GN <ocg0229@gmail.com>
2025-01-14 22:23:06 +0900

    FEAT: 페이지 직렬화 설정 추가
    - Page 객체의 안정적인 JSON 직렬화를 위한 설정 추가
    - Spring Data의 pageSerializationMode를 VIA_DTO로 설정하여 일관된 API 응답 구조 보장

--- src/main/java/org/example/paginationpractice/PaginationPracticeApplication.java
+++ src/main/java/org/example/paginationpractice/PaginationPracticeApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-PaginationPracticeApplication {
+public class PaginationPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaginationPracticeApplication.class, args);
